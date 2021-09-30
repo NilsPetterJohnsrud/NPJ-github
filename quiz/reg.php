e857f47485209c1009fd0fa1394deafd97c94390
NilsPetterJohnsrud <91133191+NilsPetterJohnsrud@users.noreply.github.com>
2021-09-30 12:47:08 +0200

    start

--- quiz/reg.php
+++ quiz/reg.php
@@ -1,20 +1,20 @@
 <!doctype html>
-<html>
+<html lang="no">
 <head>
-<meta charset="utf-8">
-<title>Nils Petter Johnsrud</title>
-	<link rel="stylesheet" type="text/css" href="meg.css" />
-	<link rel="icon" type="image/png" href="Bilder/NPJ-logo-B.png" sizes="16x16">
+    <meta charset="utf-8"/>
+    <title>Søvnregistrering</title>
+	<link rel="stylesheet" type="text/css" href="../meg.css" />
+	<link rel="icon" type="image/png" href="../Bilder/NPJ-logo-B.png" sizes="16x16">
+	<link href="../meg.css" rel="stylesheet" type="text/css">
 
 </head>
-
 <body>
-	<div id="wrapper">
+<div id="wrapper">
 		<div id="box1">
 	<table width="100%" border="0" cellspacing="5" cellpadding="5" vertical-align: midle><!--Tabell som bruker 100% av siden uten border og med midtstilling vertikalt-->
   <tbody>
     <tr>
-      <td>&nbsp; <a href="index.html"> <img src="Bilder/NPJ-logo-B.png" alt="Nils Petters logo" width="250px" height="auto" /> </a> </td>
+      <td>&nbsp; <a href="../index.html"> <img src="../Bilder/NPJ-logo-B.png" alt="Nils Petters logo" width="250px" height="auto" /> </a> </td>
       <td>&nbsp; <h0>Nils Petter Johnsrud</h0></td> 
     </tr>
   </tbody>
@@ -37,10 +37,29 @@
 			<!-- kontaktMeg.html -->
 		</nav></div>
 	<div id="box3">
-	<h1>Min nettside knyttet til undervisning</h1><!-- Her skal nettsidens innhold inn -->
+	<h1>Oppgave med PHP-registrering av søvn til tekstfil</h1>
+		<p></p>
+
+ <h1>Søvnregistrering</h1>
+<?php
+$fil = fopen("sovn.csv","a") // Åpne fil
+   or die("Kan ikke åpne fil");
+
+foreach ($_GET as $key => $value) // Gå gjennom alle input-data
+{
+  echo "<p> $key : \t $value";    // Skriv inkomne data på skjermen
+  fwrite($fil,$value);            // Skriv til fil
+  fwrite($fil,";");               // Sett inn et semikolon imellom
+}
+fwrite($fil,"\n");                // Avslutt med linjeskift
+fclose($fil);                     // Lukk fila
+ ?>
+
+	<!-- Her skal nettsidens innhold inn -->
 		</div>
 		<div id="box4">Nils Petter Johnsrud - nilsj@viken.no</div>
 	</div>
 
-</body>
+
+ </body>
 </html>
---
+++ quiz/sovnreg.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Oppgave søvnregistrering</title>
+	<link rel="stylesheet" type="text/css" href="../meg.css" />
+	<link rel="icon" type="image/png" href="../Bilder/NPJ-logo-B.png" sizes="16x16">
+
+</head>
+
+<body>
+<div id="wrapper">
+		<div id="box1">
+	<table width="100%" border="0" cellspacing="5" cellpadding="5" vertical-align: midle><!--Tabell som bruker 100% av siden uten border og med midtstilling vertikalt-->
+  <tbody>
+    <tr>
+      <td>&nbsp; <a href="../index.html"> <img src="../Bilder/NPJ-logo-B.png" alt="Nils Petters logo" width="250px" height="auto" /> </a> </td>
+      <td>&nbsp; <h0>Nils Petter Johnsrud</h0></td> 
+    </tr>
+  </tbody>
+</table>
+	</div>
+	<div id="box2">
+		<nav>
+			<a href="prosjekter.html" class="knapp">Prosjekter</a>
+			<!-- prosjekter.html -->
+			<a href="bilder.html" class="knapp">Bilder</a>
+			<!-- bilder.html - laget -->
+			<a href="opplering.html" class="knapp">Opplæring</a><!-- opplering.html -->
+			<a href="artikler.html" class="knapp">Artikler</a>
+			<!-- artikler.html -->
+			<a href="lenkesamling.html" class="knapp">Lenkesamling</a>
+			<!-- lenkesamling.html-->
+			<a href="omMeg.html" class="knapp">Om Meg</a>
+			<!-- omMeg.html -->
+			<a href="kontaktMeg.html" class="knapp">Kontakt meg</a>
+			<!-- kontaktMeg.html -->
+		</nav></div>
+	<div id="box3">
+	<h1>Oppgave med PHP-registrering av søvn til tekstfil</h1>
+		<p></p>
+
+	 <h1>Quiz</h1>
+
+   <form method="GET" action="reg.php">
+
+    <dl>
+      <dt>Navn</dt>
+      <dd><input type="text" name="Navn" /></dd>
+		<dt>Mandag</dt>
+		<dd><input type="number" name="Mandag"</dd>
+		<dt>Tirsdag</dt>
+		<dd><input type="number" name="Tirsdag"</dd>
+		<dt>Onsdag</dt>
+		<dd><input type="number" name="Onsdag"</dd>
+		<dt>Torsdag</dt>
+		<dd><input type="number" name="Torsdag"</dd>
+		<dt>Fredag</dt>
+		<dd><input type="number" name="Fredag"</dd>
+		<dt>Lørdag</dt>
+		<dd><input type="number" name="Lørdag"</dd>
+		<dt>Søndag</dt>
+		<dd><input type="number" name="Søndag"</dd>
+ </dl>
+    <p><input type="submit"  value="registrer"/>
+   </form>
+
+	<!-- Her skal nettsidens innhold inn -->
+		</div>
+		<div id="box4">Nils Petter Johnsrud - nilsj@viken.no</div>
+	</div>
+
+</body>
+</html>
