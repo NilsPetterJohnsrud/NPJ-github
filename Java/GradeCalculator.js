f419c85885a30159235c75d24df9a45db137c322
NilsPetterJohnsrud <91133191+NilsPetterJohnsrud@users.noreply.github.com>
2021-10-01 11:22:42 +0200

    1 okt

---
+++ Java/GradeCalculator.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+
+const studentOne = {
+  name: "Nils Petter",
+  testGrade: 88,
+  assignmentGrade: 91,
+  examGrade: 80
+}
+
+const studentTwo = {
+  name: "Vidar",
+  testGrade: 75,
+  assignmentGrade: 70,
+  examGrade: 85
+}
+
+const studentTree = {
+  name: "Dewald",
+  testGrade: 81,
+  assignmentGrade: 92,
+  examGrade: 75
+}
+
+const studentFour = {
+  name: "Sean",
+  testGrade: 67,
+  assignmentGrade: 55,
+  examGrade: 70
+}
+
